bfe3e5675450e339c2afb144653fd7b01e18ed4f
KyleHeLi <hli162@uottawa.ca>
2017-01-10 15:03:39 -0500

    fix the conflict problem in Reverse_Integer.py

--- LeetCode/2_Reverse_Integer.py
+++ LeetCode/2_Reverse_Integer.py
@@ -1,116 +1,54 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
-class Solution( object ):
-	def reverse( self, x ):
-		"""
-		:type x: int
-		:rtype: int
-		"""
-		xStr = str( abs( x ) )
-		xList = list( xStr )
-		xLen = len(xList)
-		MAXLEN = 2**31
 
-		if x > MAXLEN or x == 0:
-			return 0
-		else:
-			for i in range( xLen ):
-				if xList[-1] == '0':
-					xList.pop()
-				else:
-					break
+class Solution(object):
 
-		xLen = len( xList )
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        xStr = str(abs(x))
+        xList = list(xStr)
+        xLen = len(xList)
+        MAXLEN = 2**31
 
-		for i in range( xLen / 2 ):
-			xList[i], xList[-(i+1)] = xList[-(i+1)], xList[i]
+        if x > MAXLEN or x == 0:
+            return 0
+        else:
+            for i in range(xLen):
+                if xList[-1] == '0':
+                    xList.pop()
+                else:
+                    break
 
-		if x >= 0:
-			result = int( ''.join( xList ) )
-		else:
-			result = -int( ''.join( xList ) )
+        xLen = len(xList)
 
-		if result > MAXLEN:
-			return 0
-		else:
-			return result
+        for i in range(xLen / 2):
+            xList[i], xList[-(i + 1)] = xList[-(i + 1)], xList[i]
 
+        if x >= 0:
+            result = int(''.join(xList))
+        else:
+            result = -int(''.join(xList))
+
+        if result > MAXLEN:
+            return 0
+        else:
+            return result
 
 
 if __name__ == '__main__':
 
-	value1 = 123
-	value2 = -123
-	value3 = 1000000003
-	value4 = 10000
-	value = 0
-	test = Solution()
-	print test.reverse( value1 )
-	print test.reverse( value2 )
-	print test.reverse( value3 )
-	print test.reverse( value4 )
-	print test.reverse( value4 )
-=======
-=======
->>>>>>> 221a1bab85b6aa034f6b0658e5364eedd8183a33
-#!/usr/bin/env python
-# -*- encoding:utf-8 -*-
-
-class Solution( object ):
-	def reverse( self, x ):
-		"""
-		:type x: int
-		:rtype: int
-		"""
-		xStr = str( abs( x ) )
-		xList = list( xStr )
-		xLen = len(xList)
-		MAXLEN = 2**31
-
-		if x > MAXLEN or x == 0:
-			return 0
-		else:
-			for i in range( xLen ):
-				if xList[-1] == '0':
-					xList.pop()
-				else:
-					break
-
-		xLen = len( xList )
-
-		for i in range( xLen / 2 ):
-			xList[i], xList[-(i+1)] = xList[-(i+1)], xList[i]
-
-		if x >= 0:
-			result = int( ''.join( xList ) )
-		else:
-			result = -int( ''.join( xList ) )
-
-		if result > MAXLEN or result < -MAXLEN:
-			return 0
-		else:
-			return result
-
-
-
-if __name__ == '__main__':
-
-	value1 = 123
-	value2 = -123
-	value3 = 1000000003
-	value4 = 10000
-	value = 0
-	test = Solution()
-	print test.reverse( value1 )
-	print test.reverse( value2 )
-	print test.reverse( value3 )
-	print test.reverse( value4 )
-	print test.reverse( value4 )
-<<<<<<< HEAD
->>>>>>> 221a1bab85b6aa034f6b0658e5364eedd8183a33
-=======
->>>>>>> 221a1bab85b6aa034f6b0658e5364eedd8183a33
-	print test.reverse( value )
+    value1 = 123
+    value2 = -123
+    value3 = 1000000003
+    value4 = 10000
+    value = 0
+    test = Solution()
+    print test.reverse(value1)
+    print test.reverse(value2)
+    print test.reverse(value3)
+    print test.reverse(value4)
+    print test.reverse(value)
