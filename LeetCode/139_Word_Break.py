8c859c6e082dddcac6ccd72d39d830c9df33dce6
KyleHeLi <hli162@uottawa.ca>
2017-01-26 16:28:29 -0500

    Add new solution wordBreak1()

--- LeetCode/139_Word_Break.py
+++ LeetCode/139_Word_Break.py
@@ -30,6 +30,12 @@ class Solution(object):
                     d[i] = True
         return d[-1]
 
+    def wordBreak1(self, s, words):
+        ok = [True]
+        for i in range(1, len(s) + 1):
+            ok += any(ok[j] and s[j:i] in words for j in range(i)),
+        return ok[-1]
+
 
 def main():
     testStr0 = "leetcode"
