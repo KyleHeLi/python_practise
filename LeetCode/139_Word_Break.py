f36dd0107724389abd85c594484e6cfd63979b79
KyleHeLi <hli162@uottawa.ca>
2017-01-26 15:52:35 -0500

    Solution to Word Break

--- LeetCode/139_Word_Break.py
+++ LeetCode/139_Word_Break.py
@@ -1,11 +1,86 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+'''
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words. You may assume the dictionary does not contain duplicate words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+'''
+
+
 class Solution(object):
+
     def wordBreak(self, s, wordDict):
         """
         :type s: str
-        :type wordDict: List[str]
+        :type wordDict:
+         List[str]
         :rtype: bool
         """
-        
+        if not s or not wordDict:
+            return False
+        d = [False] * len(s)
+        for i in range(len(s)):
+            for word in wordDict:
+                if word == s[i - len(word) + 1:i + 1] and (d[i - len(word)] or i - len(word) == -1):
+                    d[i] = True
+        return d[-1]
+
+    def wordBreak1(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict:
+         List[str]
+        :rtype: bool
+        """
+        if not s or not wordDict:
+            return False
+        d = [False] * len(s)
+        i = 0
+        while i != len(s):
+            for word in wordDict:
+
+                # if word == s[i - len(word) + 1:i + 1] and (d[i - len(word)] or i - len(word) == -1):
+                #     d[i] = True
+            i += 1
+        return d[-1]
+
+
+def main():
+    testStr0 = "leetcode"
+    testDict0 = ['leet', 'code', 'good']
+
+    testStr = "gooday"
+    testStr2 = "goodbye"
+    testStr3 = "goodday"
+    testDict = ['good', 'day']
+    testDict2 = ['good']
+    test = Solution()
+
+    # print test.wordBreak(testStr0, testDict0)
+    # print("================")
+    # print test.wordBreak(testStr, testDict)
+    # print test.wordBreak(testStr2, testDict)
+    # print test.wordBreak(testStr3, testDict)
+    # print("================")
+    # print test.wordBreak(testStr, testDict2)
+    # print test.wordBreak(testStr2, testDict2)
+    # print test.wordBreak(testStr3, testDict2)
+
+    print test.wordBreak1(testStr0, testDict0)
+    print("================")
+    print test.wordBreak1(testStr, testDict)
+    print test.wordBreak1(testStr2, testDict)
+    print test.wordBreak1(testStr3, testDict)
+    print("================")
+    print test.wordBreak1(testStr, testDict2)
+    print test.wordBreak1(testStr2, testDict2)
+    print test.wordBreak1(testStr3, testDict2)
+
+
+if __name__ == '__main__':
+    main()
